a722498fdee532d4f50a011b4ea5455b267e2278
Mohammad Jahanbakhsh <mj74911@gmail.com>
2023-08-03 13:17:32 +0330

    fix madule files

---
+++ format1.py
@@ -0,0 +1,3 @@
+def format(*x):
+  # pass two argomans
+  return "x1 :{} , x2{}".format(x[0],x[1])
---
+++ my_sqrt.py
@@ -0,0 +1,4 @@
+from numpy import sqrt
+
+def func_sqrt (*List):
+  return list (map (sqrt , List))
